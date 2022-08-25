497db452512cfd26c9244891fd63fd7eb71ddd52
Jia Song <j.song0320ustc@gmail.com>
2022-08-24 21:57:47 -0700

    adding class

--- src/CodeDemo.cpp
+++ src/CodeDemo.cpp
@@ -1,12 +1,18 @@
+#include <vector>
 #include <iostream>
 #include <string>
 #include "cow.h"
+#include "records.h"
 
 using namespace std;
 
 int a = 37;
 int *ptr;
 cow *my_cow;
+vector<int> primes;
+Student my_student(1,"Hilda Jones");
+Course my_course(7,"Physics 101",4);
+Grade my_grade(1,7,'B');
 
 int main(){
     ptr = &a;
@@ -23,5 +29,27 @@ int main(){
     cout << typeid(ptr).name() << endl;
     cout << typeid(my_cow).name() << endl;
     delete my_cow;
+    cout << "The vector has " << primes.size() << " elements." << endl;
+    primes.push_back(2);
+    primes.push_back(3);
+    primes.push_back(4);
+    primes.push_back(5);
+    primes.push_back(7);
+    cout << "The vector has " << primes.size() << " elements." << endl;
+    cout << "The element at index 2 is " << primes[2] << endl;
+    primes[2] = 13;
+    cout << "The element at index 2 is " << primes[2] << endl;
+    vector<cow> cattle;
+    cattle.push_back(cow("better", 6, meat));
+    cattle.push_back(cow("Libby", 4, pet));
+    cattle.push_back(cow("Trudy", 5, dairy));
+    cout << "The first cow is " << cattle.begin()->get_name() << endl;
+    cout << "At index 1 we have " << cattle[1].get_name() << endl;
+    cout << "Next to last is " << prev(cattle.end(), 2)->get_name() << endl;
+    cout << "the last cow is " << (cattle.end() - 1)->get_name() << endl;
+    cout << "Student: " << my_student.get_name() << endl;
+    cout << "Course: " << my_course.get_name() << endl;
+    cout << "Credits: " << my_course.get_credits() << endl;
+    cout << "Grade: " << my_grade.get_grade() << endl;
     return (0);
 }
